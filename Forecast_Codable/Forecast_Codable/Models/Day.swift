ea24979e511f2b1e97b8e9d991eb9974c88f4d48
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-20 14:04:28 -0600

    finished model

---
+++ Forecast_Codable/Forecast_Codable/Models/Day.swift
@@ -0,0 +1,41 @@
+//
+//  Day.swift
+//  Forecast_Codable
+//
+//  Created by Colton Brenneman on 6/20/23.
+//
+
+import Foundation
+
+struct TopLevelDictionary: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case days = "data"
+        case cityName = "city_name"
+    }
+    let days: [Day]
+    let cityName: String
+}
+
+struct Day: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case temp
+        case highTemp = "high_temp"
+        case lowTemp = "low_temp"
+        case validDate = "valid_date"
+        case weather
+    }
+    let temp: Double
+    let highTemp: Double
+    let lowTemp: Double
+    let validDate: String
+    let weather: Weather
+}
+
+struct Weather: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case description
+        case iconString = "icon"
+    }
+    let description: String
+    let iconString: String
+}
